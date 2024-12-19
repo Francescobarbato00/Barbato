3faaa75f80004b2f53b01344c41bd1376fb27a53
Francescobarbato00 <francesco.barbato24@gmail.com>
2024-12-19 10:57:41 +0100

    a

--- src/pages/components/BlogGrid.jsx
+++ src/pages/components/BlogGrid.jsx
@@ -73,12 +73,13 @@ const BlogGrid = () => {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10 bg-white">
       {/* Titolo Blog */}
-      <div className="text-center">
-        <h1 className="text-3xl md:text-5xl font-bold text-blue-600">Our Blog</h1>
-        <p className="text-gray-600 mt-2 text-sm md:text-lg">
-          Insights, strategies, and stories to grow your business and succeed.
-        </p>
-      </div>
+      <div className="text-center fade-in-up">
+  <h1 className="text-3xl md:text-5xl font-bold text-blue-600">Our Blog</h1>
+  <p className="text-gray-600 mt-2 text-sm md:text-lg">
+    Insights, strategies, and stories to grow your business and succeed.
+  </p>
+</div>
+
 
       {/* Filtri Categoria */}
       <div className="flex flex-wrap justify-center gap-2 md:gap-4 mt-8 mb-10">
